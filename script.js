565213cb603ee8fe980070e81bc3435799e94101
Elvira <djeimikemp@yandex.ru>
2024-09-10 13:43:58 +0300

    added smooth transform of photo

--- script.js
+++ script.js
@@ -6,40 +6,49 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.json())
         .then(data => {
             config = data;
+            setInterval(() => {
+                changePhoto('next');
+            }, 10000);
         });
 });
 
 function changePhoto(action) {
     if (!config) return;
 
-    let photo = document.querySelector("#slideshow_container img");
-
-    switch (action) {
-        case "next":
-            if (flag + 1 < config.slideshow.length) {
-                let value = config.slideshow[flag + 1].split(', ');
-                photo.src = value[0];
-                photo.alt = value[1];
-                ++flag;
-            } else {
-                let value = config.slideshow[0].split(', ');
-                photo.src = value[0];
-                photo.alt = value[1];
-                flag = 0;
-            }
-            break;
-        case "prev":
-            if (flag - 1 >= 0) {
-                let value = config.slideshow[flag - 1].split(', ');
-                photo.src = value[0];
-                photo.alt = value[1];
-                --flag;
-            } else {
-                let value = config.slideshow[config.slideshow.length - 1].split(', ');
-                photo.src = value[0];
-                photo.alt = value[1];
-                flag = config.slideshow.length - 1;
-            }
-            break;
-    }
+    let photo = document.querySelector("#slideshow_image");
+    
+    photo.style.opacity = 0;
+    photo.style.transform = 'scale(0.9)';
+    setTimeout(() => {
+        switch (action) {
+            case "next":
+                if (flag + 1 < config.slideshow.length) {
+                    let value = config.slideshow[flag + 1].split(', ');
+                    photo.src = value[0];
+                    photo.alt = value[1];
+                    ++flag;
+                } else {
+                    let value = config.slideshow[0].split(', ');
+                    photo.src = value[0];
+                    photo.alt = value[1];
+                    flag = 0;
+                }
+                break;
+            case "prev":
+                if (flag - 1 >= 0) {
+                    let value = config.slideshow[flag - 1].split(', ');
+                    photo.src = value[0];
+                    photo.alt = value[1];
+                    --flag;
+                } else {
+                    let value = config.slideshow[config.slideshow.length - 1].split(', ');
+                    photo.src = value[0];
+                    photo.alt = value[1];
+                    flag = config.slideshow.length - 1;
+                }
+                break;
+        }
+        photo.style.opacity = 1;
+        photo.style.transform = 'scale(1)'; 
+    }, 500); 
 }
