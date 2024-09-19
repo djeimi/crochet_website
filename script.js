6e5b059e04c1d9ef78c53c314e159d3a15ced1f5
Elvira <djeimikemp@yandex.ru>
2024-09-19 18:03:48 +0300

    added functional for the menu in the left side + after the update of cards on the page - scroll to top

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let flag = 0;
 let config;
+let currentFilterPage = 1;
+const itemsPerPage = 8;
+let currentFilter;
+let filterSwitchedOn = false;
 
 document.addEventListener('DOMContentLoaded', function() {
     fetch('config.json')
         .then(response => response.json())
         .then(data => {
             config = data;
-        });
+            updateCards(1);
+            updateNavigation(1);
+        })
+        setInterval(() => {
+            changePhoto('next');}, 
+            10000);
 
     let cards = document.querySelector('.cards');
     let navLinks = document.querySelectorAll('.list_nav a');
@@ -32,6 +41,8 @@ document.addEventListener('DOMContentLoaded', function() {
             `;
             cards.appendChild(cardItem);
         });
+
+        scrollToTop();
     }
 
     function updateNavigation(page) {
@@ -119,7 +130,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
                 list.appendChild(firstLinkItem);
                 list.appendChild(dotsItemLeft);
-                
+
                 for (let i = navLinks[navLinks.length - 1].textContent - initialNavLinksLength + 2; i <= lastPage; i++){
                     let linkItem = document.createElement('li');
                     linkItem.innerHTML = `
@@ -159,15 +170,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    document.querySelector('.list_nav').addEventListener('click', function(event) {
-        if (event.target.tagName === 'A') {
-            event.preventDefault();
-            const page = event.target.textContent;
-            updateCards(page);
-            updateNavigation(page);
-        }
-    });
-
     function navigateTo(element){
         let currentPage = document.querySelector('.current');
         let newPage;
@@ -181,22 +183,52 @@ document.addEventListener('DOMContentLoaded', function() {
         updateCards(newPage);
         updateNavigation(newPage);
     }
+    
+    document.querySelector('.list_nav').addEventListener('click', function(event) {
+        if (event.target.tagName === 'A') {
+            const page = event.target.textContent;
+            if (!filterSwitchedOn){
+                event.preventDefault();
+                updateCards(page);
+                updateNavigation(page);
+            }
+            else{
+                if (page > currentFilterPage){
+                    currentFilterPage++;
+                }
+                else{
+                    currentFilterPage--;
+                }
+                updateCardsByFilter(currentFilter);
+            }
+        }
+    });
 
     document.getElementById('arrow_left_pages').addEventListener('click', function() {
-        navigateTo(this);
+        if (!filterSwitchedOn){
+            navigateTo(this);
+        }
     });
 
     document.getElementById('arrow_right_pages').addEventListener('click', function() {
-        navigateTo(this);
+        if (!filterSwitchedOn){
+            navigateTo(this);
+        }
+    });
+
+    document.getElementById('img_container').addEventListener('click', function() {
+        filterSwitchedOn = false;
+        
+        updateCards(1);
+        updateNavigation(1);
     });
 });
-
 
 function changePhoto(action) {
     if (!config) return;
 
     let photo = document.querySelector("#slideshow_image");
-    
+
     photo.style.opacity = 0;
     setTimeout(() => {
         switch (action) {
@@ -228,14 +260,8 @@ function changePhoto(action) {
                 break;
         }
         photo.style.opacity = 1;
-    }, 500); 
+    }, 500);
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    setInterval(() => {
-        changePhoto('next');
-    }, 10000);
-});
 
 function scrollToTop(){
     document.body.scrollIntoView({
@@ -269,3 +295,159 @@ function openMenu() {
         menuBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
     }
 }
+
+function extractPhotos(config) {
+    const photos = [];
+
+    for (const key in config) {
+        if (key.startsWith('page')) {
+            config[key].forEach(item => {
+                photos.push(item);
+            });
+        }
+    }
+
+    return photos;
+}
+
+function updateCardsByFilter(filter) {
+    let cards = document.querySelector('.cards');
+
+    currentFilter = filter;
+    filterSwitchedOn = true;
+
+    let photos = extractPhotos(config);
+
+    let filteredPhotos = photos.filter(item => {
+        let itemArray = item.split(', ');
+        let types = itemArray[2].replace('[','').replace(']','').split(' ');
+        return types.includes(filter);
+    });
+
+    let totalPages = Math.ceil(filteredPhotos.length / itemsPerPage);
+    let startIndex = (currentFilterPage - 1) * itemsPerPage;
+    let endIndex = startIndex + itemsPerPage;
+
+    cards.innerHTML = '';
+    filteredPhotos.slice(startIndex, endIndex).forEach(item => {
+        let itemArray = item.split(', ');
+        let src = itemArray[0];
+        let description = itemArray[1];
+        const cardItem = document.createElement('li');
+        cardItem.classList.add('card_item');
+        cardItem.innerHTML = `
+            <a>
+                <div class="img_container_cards">
+                    <img class="img_cards" src="${src}" alt="${description}">
+                </div>
+                <h2 class="description_cards">${description}</h2>
+            </a>
+        `;
+        cards.appendChild(cardItem);
+    });
+
+    updateFilterNavigation(totalPages);
+}
+
+function updateFilterNavigation(totalPages) {
+    let list = document.querySelector('.list_nav');
+    list.innerHTML = '';
+
+    if (totalPages <= 1) {
+        let linkItem = document.createElement('li');
+        linkItem.innerHTML = `
+            <a class="link">1</a>
+        `;
+        list.appendChild(linkItem);
+    } else if (totalPages <= 4) {
+        for (let i = 1; i <= totalPages; i++) {
+            let linkItem = document.createElement('li');
+            linkItem.innerHTML = `
+                <a class="link">${i}</a>
+            `;
+            list.appendChild(linkItem);
+        }
+    } else {
+        let firstLinkItem = document.createElement('li');
+        let dotsItemLeft = document.createElement('li');
+        let dotsItemRight = document.createElement('li');
+        let lastLinkItem = document.createElement('li');
+
+        firstLinkItem.innerHTML = `
+            <a class="link">1</a>
+        `;
+        dotsItemLeft.innerHTML = `
+            <div>...</div>
+        `;
+        dotsItemRight.innerHTML = `
+            <div>...</div>
+        `;
+        lastLinkItem.innerHTML = `
+            <a class="link">${totalPages}</a>
+        `;
+
+        list.appendChild(firstLinkItem);
+        list.appendChild(dotsItemLeft);
+
+        for (let i = currentFilterPage - 1; i <= currentFilterPage + 2 && i <= totalPages; i++) {
+            if (i > 0) {
+                let linkItem = document.createElement('li');
+                linkItem.innerHTML = `
+                    <a class="link">${i}</a>
+                `;
+                list.appendChild(linkItem);
+            }
+        }
+
+        list.appendChild(dotsItemRight);
+        list.appendChild(lastLinkItem);
+    }
+
+    let navLinks = document.querySelectorAll('.list_nav a');
+    navLinks.forEach(link => {
+        if (link.textContent === currentFilterPage.toString()) {
+            link.parentElement.classList.add('current');
+        } else {
+            link.parentElement.classList.remove('current');
+        }
+    });
+
+    let leftArrow = document.getElementById('arrow_left_pages');
+    let rightArrow = document.getElementById('arrow_right_pages');
+
+    if (currentFilterPage != 1){
+        leftArrow.style.visibility = 'visible';
+    }
+    else{
+        leftArrow.style.visibility = 'hidden';
+    }
+
+    if (currentFilterPage == totalPages){
+        rightArrow.style.visibility = 'hidden';
+    }
+    else{
+        rightArrow.style.visibility = 'visible';
+    }
+}
+
+document.getElementById('arrow_left_pages').addEventListener('click', function() {
+    if (currentFilterPage > 1) {
+        currentFilterPage--;
+        updateCardsByFilter(currentFilter);
+    }
+});
+
+document.getElementById('arrow_right_pages').addEventListener('click', function() {
+    let photos = extractPhotos(config);
+    let filteredPhotos = photos.filter(item => {
+        let itemArray = item.split(', ');
+        let types = itemArray[2].replace('[','').replace(']','').split(' ');
+        return types.includes(currentFilter);
+    });
+    let totalPages = Math.ceil(filteredPhotos.length / itemsPerPage);
+
+    if (currentFilterPage < totalPages) {
+        currentFilterPage++;
+        updateCardsByFilter(currentFilter);
+    }
+});
